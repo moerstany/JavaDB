0741ec8c0c42befe817f05fb6962f7a578227508
moerstany <moerstany@gmail.com>
2023-01-21 13:19:46 +0700

    java+database mysql

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class Main {
     public static void main(String[] args) {
-        try {
+        /*try {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             //System.out.println("Connect");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/MyDb","root","123123");
@@ -14,8 +16,26 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Not Connect");
             throw new RuntimeException(e);
-        }
+        }*/
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
 
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/MyDb", "root", "123123")) {
+                System.out.println("Connect");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM Persons");
+                while (resultSet.next()) {
+                    System.out.print(resultSet.getString(1));
+                    System.out.print(resultSet.getString(2));
+                    System.out.print(resultSet.getInt(3));
+                }
+            }
+
+        }
+        catch (Exception e) {
+            System.out.println("Not Connect");
+            throw new RuntimeException(e);
+        }
     }
 
 }
